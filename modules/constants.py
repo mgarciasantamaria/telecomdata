d7e5aa41f5f855951959901e06202b2e9e00cf88
Mario Garcia <mandres_garcia@hotmail.com>
2023-07-13 12:40:14 -0500

    Se modifica el destino de la base de datos

--- modules/constants.py
+++ modules/constants.py
@@ -2,7 +2,7 @@
 #_*_ codig: utf8 _*_
 json_path='./status.json'
 log_Path="./logs" # Ruta del folder donde se alojan los archivos logs
-data_base_connect_prod="host=10.10.130.38 dbname=cdndb user=vodtransfers3 password=vod-2022" #Datos para establecer conexion con la base de datos de produccion.
+data_base_connect_prod="host=10.10.130.152 dbname=toolboxprod user=vodaplications password=V0D-20234pl1c4t10ns" #Datos para establecer conexion con la base de datos de produccion.
 aws_profile='pythonapps' #Nombre del perfil de aconexion a AWS
 Downloads_Path="./S3Download"
 Bucket_logs='logs-telecom-arg' #Use (functions:Dowload_Logs)
